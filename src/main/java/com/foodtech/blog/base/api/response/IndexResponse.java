3532f16b5f56b01029a3af3b260e6effecfc9bfa
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-07-08 01:24:35 +0300

    L001

---
+++ src/main/java/com/foodtech/blog/base/api/response/IndexResponse.java
@@ -0,0 +1,12 @@
+package com.foodtech.blog.base.api.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class IndexResponse {
+    private String message;
+}
---
+++ src/main/java/com/foodtech/blog/base/routers/BaseApiRoutes.java
@@ -0,0 +1,8 @@
+package com.foodtech.blog.base.routers;
+
+public class BaseApiRoutes {
+    private static final String ROOT ="/api";
+    public static final String V1 = ROOT+"/v1";
+    public static final String V2 = ROOT+"/v2";
+
+}
